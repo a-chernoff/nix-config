49524cda2bb24f509a893b1695d29d20519d5d22
Alexander Chernoff <alex@chernoff.xyz>
2025-04-23 00:18:01 +0000

    caches: add nix-community cachix

--- modules/nixos/shared/caches.nix
+++ modules/nixos/shared/caches.nix
@@ -1,5 +1,9 @@
 {
   # No extra substituters for now
-  nix.settings.trusted-public-keys = [ ];
-  nix.settings.substituters = [ ];
+  nix.settings.extra-substituters = [
+    "https://nix-community.cachix.org"
+  ];
+  nix.settings.extra-trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
 }
